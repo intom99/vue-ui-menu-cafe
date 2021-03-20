e34d1b6a8e387cf582814ee3d360ed291e52a088
intom <iin.arahmah99@gmail.com>
2021-03-20 20:57:00 +0700

    add: fitur search

--- src/views/Drinks.vue
+++ src/views/Drinks.vue
@@ -6,7 +6,29 @@
         <div class="col">
           <h2 class="mt-4">List of <strong>Drink</strong> :</h2>
         </div>
+        <div class="col">
+
+<div class="input-group mt-4">
+                <input v-model="search"
+          type="text"
+          class="form-control"
+          placeholder="Search Drink"
+          aria-label="Search"
+          aria-describedby="basic-addon1"
+          @keyup="searchFood"
+        />
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">
+            <b-icon-search></b-icon-search>
+          </span>
+        </div>
       </div>
+
+        </div>
+      </div>
+
+      
+      <hr />
 
       <div class="row mb-4">
         <div
@@ -16,8 +38,7 @@
         >
           <Product :product="product" />
         </div>
-      </div>   
-
+      </div>
     </div>
   </div>
 </template>
@@ -37,11 +58,18 @@ export default {
   data() {
     return {
       products: [],
+      search:'',
     };
   },
   methods: {
     setProduct(data) {
       this.products = data;
+    },
+    searchFood(){
+          axios
+      .get("http://localhost:3000/drink?q="+this.search)
+      .then((response) => this.setProduct(response.data))
+      .catch((error) => console.log("Gagal : ", error));
     },
   },
   mounted() {
