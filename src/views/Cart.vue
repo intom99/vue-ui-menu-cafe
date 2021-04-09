4e1fad788a9d514b200e71640a1f5121906221cc
intom <iin.arahmah99@gmail.com>
2021-04-09 23:54:52 +0700

    add : view cart

---
+++ src/views/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="Cart">
+<Navbar/>
+
+</div>
+  
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+    name: 'Cart',
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
